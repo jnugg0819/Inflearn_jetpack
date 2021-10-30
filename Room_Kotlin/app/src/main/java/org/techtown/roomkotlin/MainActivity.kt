12a3ec6220f108500649874d0e88f3e4f0d76cbe
Olio <jgg0819@nate.com>
2021-10-30 23:12:19 +0900

    separate UI and logic with ViewModel

--- Room_Kotlin/app/src/main/java/org/techtown/roomkotlin/MainActivity.kt
+++ Room_Kotlin/app/src/main/java/org/techtown/roomkotlin/MainActivity.kt
@@ -3,27 +3,30 @@ package org.techtown.roomkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "database-name"
-        ).allowMainThreadQueries().build()
+        val viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
 
         //Db안에있는 값을 관찰중 그리고 변경사항이 있는지 확인후 텍스트에다 뿌려줍니다. 밑에 주석처리한 코드랑 같은거겠죠.
-        db.todoDao().getAll().observe(this, Observer { todos ->
+        viewModel.getAll().observe(this, Observer { todos ->
             result_text.text = todos.toString()
         })
 
-        result_text.text = db.todoDao().getAll().toString()
         add_button.setOnClickListener {
-            db.todoDao().insert(Todo(todo_edit.text.toString()))
+            lifecycleScope.launch(Dispatchers.IO){
+                viewModel.insert(Todo(todo_edit.text.toString()))
+            }
 //            result_text.text = db.todoDao().getAll().toString() 여기서 번거롭게 디비에값을 불러와줌.
         }
 
---
+++ Room_Kotlin/app/src/main/java/org/techtown/roomkotlin/MainViewModel.kt
@@ -0,0 +1,22 @@
+package org.techtown.roomkotlin
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = Room.databaseBuilder(
+        application,
+        AppDatabase::class.java, "database-name"
+    ).allowMainThreadQueries().build()
+
+    fun getAll(): LiveData<List<Todo?>?> {
+        return db.todoDao().getAll()
+    }
+
+    suspend fun insert(todo: Todo){ //suspend coroutine안으로 무조건 넣어달라는뜻.
+        db.todoDao().insert(todo)
+    }
+}
