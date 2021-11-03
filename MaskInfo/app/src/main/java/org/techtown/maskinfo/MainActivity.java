45956e5176db7e51eb126eff18c7a415449d302b
Olio <jgg0819@nate.com>
2021-11-04 00:29:29 +0900

    set Adapter

--- MaskInfo/app/src/main/java/org/techtown/maskinfo/MainActivity.java
+++ MaskInfo/app/src/main/java/org/techtown/maskinfo/MainActivity.java
@@ -1,8 +1,20 @@
 package org.techtown.maskinfo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.techtown.maskinfo.model.Store;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,79 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        StoreAdapter adapter = new StoreAdapter();
+        recyclerView.setAdapter(adapter);
+
+        List<Store> items = new ArrayList<>();
+        Store store = new Store();
+        store.setAddr("asdfasdf");
+        store.setName("우리 약국");
+        items.add(store);
+        items.add(store);
+        items.add(store);
+        items.add(store);
+        items.add(store);
+
+        adapter.updateItems(items);
     }
+}
+
+class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreViewHolder>{
+
+    private List<Store> mItems = new ArrayList<>();
+
+    //아이템 뷰 정보를 가지고 있는 클래스.
+    static class StoreViewHolder extends RecyclerView.ViewHolder{
+
+        TextView nameTextView;
+        TextView addressTextView;
+        TextView distanceTextView;
+        TextView remainTextView;
+        TextView countTextView;
+
+        public StoreViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            nameTextView = itemView.findViewById(R.id.name_test_view);
+            addressTextView = itemView.findViewById(R.id.addr_text_view);
+            distanceTextView = itemView.findViewById(R.id.distance_text_view);
+            remainTextView = itemView.findViewById(R.id.remain_text_view);
+            countTextView = itemView.findViewById(R.id.count_text_view);
+        }
+
+    }
+
+    public void updateItems(List<Store> items){
+        mItems = items;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public StoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_store,parent,false);
+        return new StoreViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoreViewHolder holder, int position) {
+        Store store = mItems.get(position);
+
+        holder.nameTextView.setText(store.getName());
+        holder.addressTextView.setText(store.getAddr());
+        holder.distanceTextView.setText("1.0km");
+        holder.remainTextView.setText(store.getRemainStat());
+        holder.countTextView.setText("100개이상");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+
 }
