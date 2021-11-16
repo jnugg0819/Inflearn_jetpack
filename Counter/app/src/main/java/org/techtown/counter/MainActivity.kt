07f1475fb7d422290e005d7ad07fbf8e51b3105e
Olio <jgg0819@nate.com>
2021-11-16 22:43:47 +0900

    Observe data using LiveData

--- Counter/app/src/main/java/org/techtown/counter/MainActivity.kt
+++ Counter/app/src/main/java/org/techtown/counter/MainActivity.kt
@@ -9,11 +9,12 @@ import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val viewmodel by viewModels<MainViewModel>()
+    private val viewmodel by viewModels<MainViewModel>()
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,60 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG,"onCreate: ")
 
-        counter_text.text = "${viewmodel.count}"
+        viewmodel.countLiveData.observe(this, Observer { count ->
+            counter_text.text = "$count"
+        })
 
         add_button.setOnClickListener {
-            viewmodel.count++
-            counter_text.text = "${viewmodel.count}"
+            viewmodel.increaseCount()
         }
 
         sub_button.setOnClickListener {
-            viewmodel.count--
-            counter_text.text = "${viewmodel.count}"
+            viewmodel.decreaseCount()
         }
 
-        registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks{
-            override fun onActivityCreated(p0: Activity, p1: Bundle?) {
-                Log.d(TAG,"onActivityCreated: ")
-            }
-
-            override fun onActivityStarted(p0: Activity) {
-                Log.d(TAG,"onActivityStarted: ")
-            }
-
-            override fun onActivityResumed(p0: Activity) {
-                Log.d(TAG,"onActivityResumed: ")
-            }
-
-            override fun onActivityPaused(p0: Activity) {
-                Log.d(TAG,"onActivityPaused: ")
-            }
-
-            override fun onActivityStopped(p0: Activity) {
-                Log.d(TAG,"onActivityStopped: ")
-            }
-
-            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
-                Log.d(TAG,"onActivitySaveInstanceState: ")
-            }
-
-            override fun onActivityDestroyed(p0: Activity) {
-                Log.d(TAG,"onActivityDestroyed: ")
-            }
-
-        })
-
-    }
-
-    //앱이 백그라운드에 오래둘경우 강제종료 됐을시 데이터값이 사라지는데 다시 데이터 불러오는 코드.
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", viewmodel.count)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        viewmodel.count = savedInstanceState.getInt("count")
     }
 
     companion object{
--- Counter/app/src/main/java/org/techtown/counter/MainViewModel.kt
+++ Counter/app/src/main/java/org/techtown/counter/MainViewModel.kt
@@ -1,7 +1,22 @@
 package org.techtown.counter
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var count = 0
+    private var count = 0
+        set(value) {//위에 count값이 바뀌게 되면은 value값으로 들어오게 될거야.
+            countLiveData.value = value
+            field = value //해당값을 확정짓겟다하면 field에 넣어둠.
+        }
+
+    val countLiveData = MutableLiveData<Int>()
+
+    fun increaseCount(){
+        count++
+    }
+
+    fun decreaseCount(){
+        count--
+    }
 }
