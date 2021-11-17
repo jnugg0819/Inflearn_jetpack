8f95f6afb8d826fa546f2c64779a89f8e93503d3
Olio <jgg0819@nate.com>
2021-11-18 01:21:57 +0900

    transfer data between activitys

--- dataTransfer/app/src/main/java/org/techtown/datatransfer/FirstFragment.kt
+++ dataTransfer/app/src/main/java/org/techtown/datatransfer/FirstFragment.kt
@@ -1,16 +1,33 @@
 package org.techtown.datatransfer
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
 import androidx.fragment.app.*
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()
+    )
+    {
+        imageView.setImageURI(it)
+    }
+
+    val getStartActivityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ activityResult ->
+        activityResult.data?.let { intent ->
+            intent.extras?.let { bundle ->
+                Toast.makeText(requireContext(), "result : ${bundle.getString("data","world")}" , Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 
     /*
     * 그냥 viewModels를 사용하면 Fragment에 LifeCycle에맞는 ViewModel이 생성되기때문에
@@ -21,17 +38,10 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setFragmentResultListener("requestkey"){ resultKey, bundle ->
-            var data = bundle.getString("data", "")
-
-            Toast.makeText(requireContext(),data , Toast.LENGTH_SHORT).show()
-        }
-
         button.setOnClickListener {
-
-            setFragmentResult("requestkey", bundleOf("data" to "hello"))
-//            mainViewModel.data = "Hello"
-            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+            Intent(requireContext(), ResultActivity::class.java).apply {
+                getStartActivityForResult.launch(this)
+            }
         }
     }
 }
